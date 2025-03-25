a267aea4e9f6d8c83361c912233d9b056de942d3
Tomas-Dorado <tdoradog@student.universidadunie.com>
2025-03-25 12:44:44 +0000

    realizado el ejercicio 3

---
+++ Ejercicio_3/main.py
@@ -0,0 +1,18 @@
+def magia_numerica(lista):
+    lista_sin_duplicados = list(set(lista))
+    lista_ordenada = sorted(lista_sin_duplicados, reverse=True)
+    lista_pares = [num for num in lista_ordenada if num % 2 == 0]
+    suma = sum(lista_pares)
+    nueva_lista = [suma] + lista_pares
+    
+    if suma != sum(nueva_lista[1:]):
+        raise ValueError("La suma no coincide con el primer elemento")
+    
+    return nueva_lista
+
+
+if __name__ == "__main__":
+    lista = [1, 2, 3, 4, 5, 6, 6, 7, 8, 8]
+    print(f"Lista original: {lista}")
+    resultado = magia_numerica(lista)
+    print(f"Lista procesada: {resultado}")
